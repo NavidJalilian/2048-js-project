ea55b9e98501b12b84ab2d2ef9c7da3d902a2197
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-08-03 20:10:38 +0430

    getting random tile

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import Grid from "./Grid.js";
 const gameBoard = document.querySelector(".game-board");
 const grid = new Grid(gameBoard);
+// grid.randomEmtyCell().tile=new Tile(gameBoard);
+console.log(grid.randomEmptyCell());
