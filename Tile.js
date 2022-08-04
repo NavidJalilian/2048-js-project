ff7c63be188508b83084f7a6dffa6524962eb3b2
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-08-04 20:55:01 +0430

    game is ready

--- Tile.js
+++ Tile.js
@@ -3,19 +3,25 @@ export default class Tile {
   #x;
   #y;
   #value;
-  constructor(gameBoard, value = Math.random() > 0.5 ? 2 : 4) {
+
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
-    gameBoard.append(this.#tileElement);
+    tileContainer.append(this.#tileElement);
     this.value = value;
   }
+
+  get value() {
+    return this.#value;
+  }
+
   set value(v) {
     this.#value = v;
     this.#tileElement.textContent = v;
-    const power = Math.log2(v);  
+    const power = Math.log2(v);
     const backgroundLightness = 100 - power * 9;
     this.#tileElement.style.setProperty(
-      "--bg-lightness",
+      "--background-lightness",
       `${backgroundLightness}%`
     );
     this.#tileElement.style.setProperty(
@@ -26,11 +32,27 @@ export default class Tile {
 
   set x(value) {
     this.#x = value;
-
     this.#tileElement.style.setProperty("--x", value);
   }
+
   set y(value) {
     this.#y = value;
     this.#tileElement.style.setProperty("--y", value);
+  }
+
+  remove() {
+    this.#tileElement.remove();
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise((resolve) => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve,
+        {
+          once: true,
+        }
+      );
+    });
   }
 }
